898d22c1717845afb3657a676cea4a4ec6a169c8
崔子琦 <oscarcui@ricequant.com>
2020-09-08 16:33:19 +0800

    bump up version

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ def readfile(filename):
 
 setup(
     name='rqrisk',
-    version='0.0.14a2',
+    version='0.0.14',
     url="https://www.ricequant.com/",
     packages=find_packages(),
     author="Ricequant",
