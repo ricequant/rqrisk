e45748ea6afd3e8fc22f7f548afdfd40f0011aed
周嘉俊 <654181984@qq.com>
2022-07-20 11:55:57 +0800

    __version__

--- rqrisk/__init__.py
+++ rqrisk/__init__.py
@@ -1,6 +1,10 @@
 from .risk import Risk
 from .utils import DAILY, WEEKLY, MONTHLY, YEARLY, NATURAL_DAILY
 
+from ._version import get_versions
+__version__ = get_versions()['version']
+del get_versions
+
 __all__ = [
     "Risk",
     "DAILY",
