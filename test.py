7d078438b3780345c69c679eb2a3af80dbd0585f
周嘉俊 <654181984@qq.com>
2023-03-01 09:23:19 +0800

    新增和修改超额计算相关指标

--- test.py
+++ test.py
@@ -235,7 +235,7 @@ def test_max_drawdown():
         assert_almost_equal(r.excess_max_drawdown, desired_excess_max_dd)
 
     _assert(volatile_returns, zero_benchmark, 0.10899999999999994, 0.10899999999999994)
-    _assert(volatile_returns, volatile_benchmark, 0.10899999999999994, 0.20000000000000007)
+    _assert(volatile_returns, volatile_benchmark, 0.10899999999999994, 0.1818181818181816)
 
 
 def test_natural_daily():
@@ -305,6 +305,19 @@ def test_ulcer_performance_index():
         assert_almost_equal(r.ulcer_performance_index, ulcer_performance_index)
 
     _assert(positive_returns, simple_benchmark, np.nan)
-    _assert(negative_returns, simple_benchmark, -0.019864148204580486)
-    _assert(weekly_returns, simple_weekly_benchamrk, -0.014606674809517502)
+    _assert(negative_returns, simple_benchmark, -0.017162734495347234)
+    _assert(weekly_returns, simple_weekly_benchamrk, 0.011223184970109419)
+
+
+def test_excess_index():
+
+    def _assert(returns, benchmark, arithmetic_excess_return, geometric_excess_return, geometric_excess_annual_return):
+        r = _r(returns, benchmark, 0)
+        assert_almost_equal(r.arithmetic_excess_return, arithmetic_excess_return)
+        assert_almost_equal(r.geometric_excess_return, geometric_excess_return)
+        assert_almost_equal(r.geometric_excess_annual_return, geometric_excess_annual_return)
+
+    _assert(one_return, one_benchmark, 0, 0, 0)
+    _assert(simple_benchmark, zero_benchmark, 0.0936852726843609, 0.09368527268436089, 11.274002099240212)
+
 
